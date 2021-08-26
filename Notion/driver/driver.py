debddefa2850247b210c0a83dabe23e6ce143b53
Axel Rasse <rasseax@gmail.com>
2021-08-26 23:37:59 +0400

    refactor: clean code and create object

--- Notion/driver/driver.py
+++ Notion/driver/driver.py
@@ -1,15 +1,14 @@
+from pandas.core.frame import DataFrame
 import requests
 import pandas as pd
 import json
-from pprint import pprint
-from helper import extract_property, insert_property
+from helper import PropertyObject, BlockObject, PageObject
+from helper import (
+    unstack_properties,
+)
 
 # Notion version
 _VERSION = "2021-08-16"
-
-
-def get_id_from_page_url(page_url):
-    return page_url.split("-")[-1]
 
 
 class Connect:
@@ -30,74 +29,171 @@ class Connect:
         pass
 
 
-class Page:
-    def __init__(self, page_url):
-        if page_url:
-            self.id = get_id_from_page_url(page_url)
+class RetreivePage:
+    """Retrieves a Page object using the ID specified."""
 
-            raw_properties = self.request()
+    def __init__(self, page_id) -> None:
+        self._page = RetreivePage.retreive(page_id)
 
-            self.properties = PageProperties(raw_properties)
-            self.content = PageContent(self)
+    def __getitem__(self, key):
+        return PropertyObject(self._page.properties[key]).extract()
 
-    def request(self):
-        url = f"https://api.notion.com/v1/pages/{self.id}"
-        res = requests.get(url, headers=_HEADERS)
+    def __setitem__(self, key, value):
+        return PropertyObject(self._page.properties[key]).insert(value)
+
+    @staticmethod
+    def retreive(page_id) -> PageObject:
+        request_url = f"https://api.notion.com/v1/pages/{page_id}"
+        res = requests.get(request_url, headers=_HEADERS)
         try:
             res.raise_for_status()
         except requests.HTTPError as e:
             return e
-        return res.json()
+        return PageObject(res.json())
 
-    def create(self, database_url):
-        pass
+    @property
+    def properties(self) -> pd.Series:
+        data = {key: self[key] for key in self._page.properties.keys()}
+        return pd.Series(data)
 
-    def update(self):
-        pass
-
-
-class PageProperties:
-    def __init__(self, raw_properties):
-        self.raw = raw_properties
-
-        # self.archived = raw_properties["archived"]
-        # self.icon = raw_properties["icon"]["emoji"]
-        # self.cover = raw_properties["cover"]
-
-    def __getitem__(self, key):
-        raw_property = self.raw["properties"][key]
-        return extract_property(raw_property)
-
-    def __setitem__(self, key, value):
-        raw_property = self.raw["properties"][key]
-        return insert_property(raw_property, value)
-
-    def show(self):
-        data = {key: self[key] for key in self.raw["properties"].keys()}
-        index = [f'{self.raw["icon"]["emoji"]}  {self["Name"]}']
-        return pd.DataFrame(data=[data], index=index).T
+    @property
+    def parent(self) -> dict:
+        parent_object = self._page.parent
+        parent_type = parent_object["type"]
+        return {parent_type: parent_object[parent_type]}
 
     def update(self):
-        # TODO: add a try catch and a validation message
-        page_id = self.raw["id"]
-        url = f"https://api.notion.com/v1/pages/{page_id}"
-        data = json.dumps({"properties": self.raw["properties"]})
+        url = f"https://api.notion.com/v1/pages/{self.id}"
+        data = json.dumps({"properties": self._page.properties})
         response = requests.patch(url, headers=_HEADERS, data=data)
         try:
             response.raise_for_status()
-            print("✨ Properties have been updated")
         except requests.HTTPError as e:
             return e
 
+        print("✨ Properties have been updated")
 
-class PageContent:
-    def __init__(self, page):
+    # def _refresh(self):
+    #     self.__init__(self._page.id)
+
+
+class RetreiveBlocks:
+    """Retrieves the content of a page using the ID specified."""
+
+    def __init__(self, page_id) -> None:
+        self._blocks = RetreiveBlocks.retreive(page_id)
+
+    @staticmethod
+    def retreive(page_id) -> list[BlockObject]:
+        url = f"https://api.notion.com/v1/blocks/{page_id}/children"
+        response = requests.get(url, headers=_HEADERS)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            return e
+        return [BlockObject(block) for block in response.json()["results"]]
+
+    @property
+    def content(self) -> pd.DataFrame:
+        content = [block.extract() for block in self._blocks]
+        return pd.DataFrame(content)
+
+
+class Page(RetreivePage, RetreiveBlocks):
+    """The Page object contains the property values and the content of a single Notion Page."""
+
+    # TODO:
+    #   1. _repr_html_ -> with Page and content
+    #   3. find a way to update the content of a page
+
+    def __init__(self, page_url) -> None:
+        self.url = page_url
+        self.id = page_url.split("-")[-1]
+
+        RetreivePage.__init__(self, self.id)
+        RetreiveBlocks.__init__(self, self.id)
+
+    def _repr_html_(self):
         pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def update(self):
+        RetreivePage.update(self)
+
+    # def _refresh(self):
+    #     return super()._refresh()
+
+    def delete(self):
+        pass
+
+    def create(self):
+        pass
+
+    @property
+    def template(self):
+        page_template = self._data_properties_properties.copy()
+
+        for useless_key in [
+            "object",
+            "id",
+            "created_time",
+            "last_edited_time",
+            "url",
+            "archived",
+        ]:
+            page_template.pop(useless_key)
+        page_template["parent"].pop("type")
+        return page_template
+
+    def duplicate(self):
+        new_page = json.dumps(self._meta_properties)
+        url = "https://api.notion.com/v1/pages"
+        response = requests.post(url, headers=_HEADERS, data=new_page)
+        return response
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            return e
+        print("✅ Your data was added to Notion")
+
+
+class Database:
+    """ """
+
+    def __init__(self, page_url) -> None:
+        self.url = page_url
+
+        self._raw_pages = self._retreive()
+        self.pages = self._clean_pages()
+
+    @property
+    def id(self):
+        path = self.url.split("/")[-1]
+        id = path.split("?")[0]
+        return id
+
+    def _retreive(self):
+        url = f"https://api.notion.com/v1/databases/{self.id}/query"
+        response = requests.post(url, headers=_HEADERS)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            return e
+        return response.json()["results"]
+
+    def _clean_pages(self) -> DataFrame:
+        df = pd.DataFrame(self._raw_pages)
+        df = unstack_properties(df)
+        return df.applymap(extract_property)
 
 
 if __name__ == "__main__":
+
     TOKEN_API = "secret_R1CrUGn8bx9itbJW0Fc9Cc0R9Lmhbnz2ayqEe0GhRPq"
     PAGE_URL = "https://www.notion.so/Tom-Simon-2ccdafe28955478b8c9d70bda0044c86"
+    DATABASE_URL = "https://www.notion.so/d0bb915c4cb4422a87679f3bb9658282?v=8cd7c6ea0ec244da9eb65aa88a87aabb"
 
     Connect(TOKEN_API)
     page = Page(PAGE_URL)
--- Notion/driver/helper.py
+++ Notion/driver/helper.py
@@ -1,83 +1,138 @@
-def extract_property(raw_property):
-    # TODO: not handling files, formula, rollup and other complex property object
-    property_type = raw_property["type"]
+class PropertyObject:
+    """An object describing by an id, type, and value of a page property."""
 
-    if property_type == "date":
-        if raw_property["date"]["end"]:
-            return f'{raw_property["date"]["start"]} -> {raw_property["date"]["end"]}'
-        return raw_property["date"]["start"]
+    def __init__(self, property_object) -> None:
+        self.id = property_object["id"]
+        self.type = property_object["type"]
+        self.value = property_object[self.type]
 
-    elif property_type in ["title", "rich_text"]:
-        texts = [text["plain_text"] for text in raw_property[property_type]]
-        return ",".join(texts)
+    def extract(self):
+        if self.value:
 
-    elif property_type == "select":
-        return raw_property["select"]["name"]
+            if self.type == "date":
+                if self.value["end"]:
+                    return f'{self.value["start"]} -> {self.value["end"]}'
+                return self.value["start"]
 
-    elif property_type == "multi_select":
-        selections = [select["name"] for select in raw_property[property_type]]
-        return ", ".join(selections)
+            elif self.type in ["title", "rich_text"]:
+                texts = [text["plain_text"] for text in self.value]
+                return ",".join(texts)
 
-    elif property_type in ["number", "url", "phone_number", "email", "checkbox"]:
-        return raw_property[property_type]
+            elif self.type == "select":
+                return self.value["name"]
 
-    elif property_type == "people":
-        return ", ".join([people["name"] for people in raw_property[property_type]])
+            elif self.type == "multi_select":
+                selections = [select["name"] for select in self.value]
+                return ", ".join(selections)
 
-    else:
-        print(property_type, "is not implemented")
-        return "Not implemented "
+            elif self.type in ["number", "url", "phone_number", "email", "checkbox"]:
+                return self.value
 
-
-def insert_property(raw_property, value):
-    # TODO: add people
-    property_type = raw_property["type"]
-
-    if property_type == "date":
-        if isinstance(value, list) and len(value) == 2:
-            raw_property["date"]["start"] = value[0]
-            raw_property["date"]["end"] = value[1]
-        elif isinstance(value, str):
-            raw_property["date"]["start"] = value
-            raw_property["date"]["end"] = None
+            elif self.type == "people":
+                peoples = [
+                    people.get("name") for people in self.value if people.get("name")
+                ]
+                return ", ".join(peoples)
         else:
-            raise TypeError(
-                "Date must be a '2021-08-28' or ['2021-08-28', '2021-10-28']"
-            )
+            return None
 
-    if property_type in ["title", "rich_text"]:
-        if isinstance(value, str):
-            del raw_property[property_type][1:]
-            raw_property[property_type][0]["plain_text"] = value
-            raw_property[property_type][0]["text"]["content"] = value
-        else:
-            raise TypeError(f"{property_type} must be a string")
+    def insert(self, value):
+        if self.type == "date":
+            if isinstance(value, list) and len(value) == 2:
+                self.value["start"] = value[0]
+                self.value["end"] = value[1]
+            elif isinstance(value, str):
+                self.value["start"] = value
+                self.value["end"] = None
+            else:
+                raise TypeError(
+                    "Date must be a '2021-08-28' or ['2021-08-28', '2021-10-28']"
+                )
 
-    if property_type == "select":
-        if isinstance(value, str):
-            raw_property[property_type] = {"name": value}
-        else:
-            raise TypeError(f"{property_type} must be a string")
+        if self.type in ["title", "rich_text"]:
+            if isinstance(value, str):
+                del self.value[1:]
+                self.value[0]["plain_text"] = value
+                self.value[0]["text"]["content"] = value
+            else:
+                raise TypeError(f"{self.type} must be a string")
 
-    if property_type == "multi_select":
-        if isinstance(value, str):
-            raw_property[property_type].clear()
-            raw_property[property_type] = [{"name": value}]
-        elif isinstance(value, list):
-            raw_property[property_type].clear()
-            for elm in value:
-                raw_property[property_type].append({"name": elm})
-        else:
-            raise TypeError(f"{property_type} must be a string or a list of string")
+        if self.type == "select":
+            if isinstance(value, str):
+                self.value = {"name": value}
+            else:
+                raise TypeError(f"{self.type} must be a string")
 
-    elif property_type == "number":
-        if isinstance(value, int):
-            raw_property[property_type] = value
-        else:
-            raise TypeError(f"{property_type} must be an integer")
+        if self.type == "multi_select":
+            if isinstance(value, str):
+                self.value.clear()
+                self.value = [{"name": value}]
+            elif isinstance(value, list):
+                self.value.clear()
+                for elm in value:
+                    self.value.append({"name": elm})
+            else:
+                raise TypeError(f"{self.type} must be a string or a list of string")
 
-    elif property_type in ["url", "phone_number", "email"]:
-        if isinstance(value, str):
-            raw_property[property_type] = value
+        elif self.type == "number":
+            if isinstance(value, int):
+                self.value = value
+            else:
+                raise TypeError(f"{self.type} must be an integer")
+
+        elif self.type in ["url", "phone_number", "email"]:
+            if isinstance(value, str):
+                self.value = value
+            else:
+                raise TypeError(f"{self.type} must be a string or a list of string")
+
+
+class PageObject:
+    """The Page object contains the property values of a single Notion page."""
+
+    def __init__(self, page_object) -> None:
+        self.properties = page_object["properties"]
+        self.parent = page_object["parent"]
+        self.icon = page_object["icon"]
+        self.cover = page_object["cover"]
+
+        self.object = page_object["object"]
+        self.id = page_object["id"]
+        self.created_time = page_object["created_time"]
+        self.last_edited_time = page_object["last_edited_time"]
+        self.archived = page_object["archived"]
+        self.url = page_object["url"]
+
+
+class BlockObject:
+    """A block object represents content within Notion. Blocks can be text, lists, media, and more. A page is a type of block, too!."""
+
+    def __init__(self, block_object) -> None:
+        self.id = block_object["id"]
+        self.type = block_object["type"]
+        self.last_edited_time = block_object["last_edited_time"]
+        self.created_time = block_object["created_time"]
+        self.value = block_object[self.type]
+
+    @property
+    def content(self):
+        if self.type == "child_page":
+            return self.value["title"]
         else:
-            raise TypeError(f"{property_type} must be a string or a list of string")
+            array_of_rich_text = self.value["text"]
+            content = [rich_text["plain_text"] for rich_text in array_of_rich_text]
+            return " ".join(content)
+
+    def extract(self):
+        return {
+            "type": self.type,
+            "content": self.content,
+            "id": self.id,
+        }
+
+
+def unstack_properties(df):
+    import pandas as pd
+
+    list_dict = [row["properties"] for _, row in df.iterrows()]
+    return pd.DataFrame(list_dict)

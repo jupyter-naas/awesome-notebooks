8204968e9c47098c15b64a0197367a904b561b85
Kanishk Pareek <knshkpareek@gmail.com>
2024-02-27 15:27:48 +0530

    some changes

--- server/controller/product-controller.js
+++ server/controller/product-controller.js
@@ -13,7 +13,7 @@ export const getProducts = async (request, response) => {
 
 export const getProductById = async (request, response) => {
     try {
-        const products = await Product.findOne({ 'id': request.params.id });
+        const products = await Product.findOne({ '_id': request.params.id });
         response.json(products);
     }catch (error) {
 

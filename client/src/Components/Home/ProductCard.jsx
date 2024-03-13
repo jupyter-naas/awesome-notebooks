8c1a44bf6e91151a0aa858baaaf8c5e3a0ef1c1e
Kanishk Pareek <knshkpareek@gmail.com>
2024-03-13 23:53:20 +0530

    some changes

--- client/src/Components/Home/ProductCard.jsx
+++ client/src/Components/Home/ProductCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { styled } from '@mui/system';
 import {Link} from 'react-router-dom'
+import flipImage from './flip.jpg'
 const CardContainer = styled('div')({
   border: '1px solid #ccc',
-  padding: 10,
-  margin: 8,
-  width: 'calc(50% - 24px)', // Adjust the width based on your design
+  padding:10,
+  margin: 0,
+  width: 'calc(50% )', // Adjust the width based on your design
   boxSizing: 'border-box',
   textAlign: 'left',
   display: 'inline-block',
   verticalAlign: 'top',
-  backgroundColor: '#f4f4f4', // Light gray background color
+  backgroundColor: '#ffffff', // Light gray background color
 });
 
 const ProductImage = styled('img')({
@@ -25,12 +26,21 @@ const Title = styled('p')({
   textOverflow: 'ellipsis',
   whiteSpace: 'nowrap',
   color: 'black',
+  fontWeight:'bold'
+});
+const Des = styled('p')({
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  color: '#818589',
+  fontSize:15
 });
 
 const Discount = styled('span')({
   color: 'green',
   fontSize: '12.5px',
   marginBottom: 10,
+  fontWeight:'bold'
 });
 
 const Price = styled('span')({
@@ -49,6 +59,12 @@ const Pr = styled('span')({
   marginBottom: 10,
   color: 'black',
 });
+const Bol=styled('span')({
+  color: 'green',
+  fontSize: '20px',
+  fontWeight:"bold",
+  marginBottom: 10,
+})
 
 const ProductCard = ({ title, price, discount, url, pr,_id }) => {
   return (
@@ -57,12 +73,14 @@ const ProductCard = ({ title, price, discount, url, pr,_id }) => {
     <CardContainer>
       <ProductImage src={url} alt={title} />
       <Title>{title.shortTitle}</Title>
-      <Discount>{price.discount}</Discount>
-      <Pr><del>₹ {price.mrp}</del></Pr>
+      <Des>{title.longTitle}</Des>
+      <Discount><Bol>↓</Bol>{price.discount} </Discount>
+      <Pr><del>₹{price.mrp} </del></Pr>
+      <Price>₹{price.cost}</Price>
+      <br />
+      <Discount>★★★★☆</Discount>
+      <img src={flipImage} alt="Flip Image" style={{ width: 70, height: 20 }} />
       <br/>
-      <Price>₹ {price.cost}</Price>
-      <br />
-      <br />
       <Pric>Free Delivery in 2 Days</Pric>
     </CardContainer>
     </Link>
